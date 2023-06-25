651da4d99a8930ee97833665f40ee860b4a93576
Prabhakar1437 <prabhakarvatsking@gmail.com>
2023-06-25 15:47:48 +0530

    DAY 1

---
+++ enum and tyoedef.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+enum day {Mon = 1, Tue=7, Wed , Thur=10 , Fri , Sat , Sun};
+int main()
+
+ 
+{
+	day d ;
+	d = Mon;
+	cout<<Mon<<endl;
+	cout<<Tue<<endl;
+	cout<<Wed<<endl;
+	cout<<Thur<<endl;
+	cout<<Fri<<endl;
+	cout<<Sat<<endl;
+	cout<<Sun<<endl;
+	return 0;
+	}
+	
+	
+
