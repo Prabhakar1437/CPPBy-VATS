083af647edd3ac07cf2b2ebf2d0f2fa7b67cb2f6
Prabhakar1437 <prabhakarvatsking@gmail.com>
2023-06-25 15:46:46 +0530

    DAY 1

---
+++ Bitwise operator.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	/*
+	int x = 11 , y = 5 , z , a , b ;
+	z = x & y;
+	a = x | y;
+	b = x ^ y;
+		cout<<z<<" "<<a<<" "<<b<<" "<<endl;
+		*/
+		int x = 20, y , z;
+		y = x<<2;
+		z = x>>2;
+		
+		cout<<y<<" "<<z<<endl;
+	return 0;
+	
+	
+}
