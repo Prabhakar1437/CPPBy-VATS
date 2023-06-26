633b63a21dec8055c0018cd6720eb6a34879557d
Prabhakar1437 <prabhakarvatsking@gmail.com>
2023-06-26 05:36:02 +0530

    DAY 2

---
+++ condinal satement1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	if(true)// we can use 1,10,-10 all give true value
+	{
+		cout<<"HELLO"<<endl;
+	}
+	else
+	{
+		cout<<"Bye"<<endl;
+	}
+	return 0;
+}
