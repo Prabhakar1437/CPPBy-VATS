31af9b8fb6cc304951cf795cc00322e6e8b6349b
Prabhakar1437 <prabhakarvatsking@gmail.com>
2023-06-26 07:39:50 +0530

    DAy 2

---
+++ Short circuit.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a = 15 , b = 5 , i=6;
+	if(ab && ++i>b)// in short circuit if first condition 
+	//is true/false they donot check the next condition
+	// is short circuit.
+	{
+	}
+	cout<<i<<endl;
+}
