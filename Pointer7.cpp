677e7aaa01b1917e403e60f199b33c89df1f032f
Prabhakar1437 <prabhakarvatsking@gmail.com>
2023-07-14 11:58:14 +0530

    Pointer

---
+++ Pointer7.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	
+	int size;
+	cout<<"Enter no. of element :";
+	cin>>size;
+	int *p = new int [size];
+	
+    
+	cout<<"Enter no. of element :";
+	cin>>size;
+	p = new int [size];
+	
+	return 0;
+}
