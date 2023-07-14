f41967212777917012222c2aa3e4c8819f5c5234
Prabhakar1437 <prabhakarvatsking@gmail.com>
2023-07-14 11:58:57 +0530

    Pointer

---
+++ Pointer8.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	
+	
+	int *p = new int [20];
+	//p[i]=[0,19];
+
+
+    //delete [p];
+    p = new int [40];
+    //p[i]=[0,39];
+	
+	return 0;
+}
